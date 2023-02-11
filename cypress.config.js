2ef60378b7ed3862d7538e72796fe2f29169ebad
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-11 18:16:07 -0500

    upgrade Cypress to v12 (#134)
    * document configuration for v10+
    
    * update actions
    
    * bump Cypress to v12
    
    * remove separate plugins file
    
    * fix the path

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+// in the user project it would be
+// const mdPreprocessor = require('cypress-markdown-preprocessor')
+const mdPreprocessor = require('.')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  viewportWidth: 500,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', mdPreprocessor)
+      return config
+    },
+    specPattern: ['cypress/e2e/*.md', 'cypress/e2e/*.js'],
+    excludeSpecPattern: ['skip.js'],
+  },
+})
